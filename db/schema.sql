a5bbc0c4c3c603b666ee1dde8c128f69174fb32d
Ethan Zander <ethanzander13@gmail.com>
2017-10-02 16:51:38 -0600

    orm.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
+
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers(
   item_id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(45) NULL,
-  devoured BOOLEAN(45) NULL,
-  price DECIMAL(10,2) NOT NULL,
-  stock_quantity INT NOT NULL,
-  product_sales Decimal(10,2) DEFAULT 0,
+  devoured BOOLEAN DEFAULT FALSE,
+  date TIMESTAMP DEFAULT now(),
   PRIMARY KEY (item_id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name)
+VALUES("The Aussie"),("BBQ Baby"),("Black & Blue"),("Holapeno");
