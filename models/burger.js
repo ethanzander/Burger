d0f8a0d16504f53e2f9fd20b1de9c1019e5855b9
Ethan Zander <ethanzander13@gmail.com>
2017-10-02 17:00:53 -0600

    burger.js

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+var orm = require("./config/orm.js");
+
+
+var burger = {
+    all: function(cb) {
+        orm.selectAll(function(results){
+            cb(results);
+        });
+    },
+
+    create: function(burgerName, cb){
+        orm.insertOne(burgerName, function(results){
+            cb(results);
+        });
+    },
+
+    update: function(selectedBurger, cb){
+        orm.updateOne(selectedBurger, function(results){
+            cb(results);
+        });
+    }
+}
+
+module.exports = burger;
